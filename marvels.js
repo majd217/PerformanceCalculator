55987afb35cc3bf212c3e0479c31aa2a185edff2
majd217 <m.amireh@gju.edu.jo>
2022-10-07 17:43:02 +0200

    delete files

--- marvels.js
+++
@@ -1,8 +0,0 @@
-const superHeroes = ['iroMan','wonderWoman','last']
-console.log(superHeroes[1])
-let sizz = superHeroes.length
-console.log(`${1+1}`,sizz)
-superHeroes.shift()
-console.log(superHeroes)
-superHeroes.unshift('something')
-console.log(superHeroes)
--- variables.js
+++
@@ -1,7 +0,0 @@
-let firstName = "majd"
-let lastName = "amireh"
-let score = 23.0
-let bonus = 5.0
-let totalScore = score + bonus;
-console.log(firstName,lastName,"scored",totalScore, "points") 
-
