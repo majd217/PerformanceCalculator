5037a18dcb3916e7b840ef27b0f14e19d300e75c
majd217 <m.amireh@gju.edu.jo>
2022-10-18 14:16:50 +0200

    Functionality done

--- main.js
+++ main.js
@@ -1,18 +1,76 @@
-var slider = document.getElementById("myRange");
-var output= document.getElementById("value");
-output.innerHTML = slider.value;
-slider.oninput = function(){
-output.innerHTML = this.value;
-}
-var x = slider.value;
-function changed(value){
-   var x = slider.value;
-   console.log(x);
-   slider.style.backgroundImage = "linear-gradient(to right,rgb(188,244,39)"+ x + "%,rgb(214,214,214)"+  +x+ "%)";
-}
 
-
+var durationSlider = document.getElementById("myRange2");
 const labels =["2022","2023","2024","2025","2026","2027","2028","2029","2030","2031","2032","2033","2034","2035","2036","2037"];
+var selectedButton = "trustbtn";
+const growbtn = document.getElementById("growbtn");
+const trustbtn = document.getElementById("trustbtn");
+const pushbtn = document.getElementById("pushbtn");
+document.getElementById(selectedButton).style.backgroundColor="rgb(214,214,214)";
+
+
+  function slidersChanged(element){
+     const filledPercentage = 100*(element.value - element.min)/(element.max-element.min); 
+     element.style.backgroundImage = "linear-gradient(to right,#B7CB75 "+ filledPercentage + "%,rgb(214,214,214)"+  filledPercentage + "%)";
+  }
+
+  function chartChanged(years){
+    const rangeValue =  labels.slice(0,years);
+    myChart.config.data.labels = rangeValue;
+    myChart.update();
+  }
+        
+  function growthSelector(element){
+    selectedButton = element.id;
+    growbtn.style.backgroundColor = "#B7CB75";
+    pushbtn.style.backgroundColor = "#B7CB75";
+    trustbtn.style.backgroundColor = "#B7CB75";
+    element.style.backgroundColor = "rgb(214,214,214)";
+    percentagePerMonth(document.getElementById("myRange").value); 
+  }
+
+
+   function percentagePerMonth(value){
+      let numOfYears = durationSlider.value;
+      console.log(numOfYears);
+      let savedAmount = (1000 * value) / 100;
+      let amountPerYear = savedAmount * 12;
+      var growthPercentage = 1.0321;
+
+      switch(selectedButton){
+        case "growbtn":
+          {
+            growthPercentage = 1.0556;
+            break;
+          }
+          case "pushbtn":
+          {
+            growthPercentage = 1.0731;
+            break;
+          }
+          
+          default:
+          case "trustbtn":
+          {
+            growthPercentage = 1.0321;
+            break;
+          }
+
+      }
+        var temp = amountPerYear;
+        myChart.config.data.datasets.forEach((dataset) => 
+        {
+          for(let i = 0 ; i < numOfYears; i++)
+          {
+            temp = growthPercentage * temp;
+            dataset.data[i] = temp;
+          }
+        });
+        myChart.update();
+      }
+
+
+        
+
 
 const data = {
   labels : labels,
