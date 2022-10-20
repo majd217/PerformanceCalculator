34acba32c04e80049937dff1be512f9dc66a62ed
majd217 <m.amireh@gju.edu.jo>
2022-10-20 12:07:59 +0200

    New calculation done (compound interest)

--- main.js
+++ main.js
@@ -1,19 +1,20 @@
 
-var durationSlider = document.getElementById("myRange2");
-const labels =["2022","2023","2024","2025","2026","2027","2028","2029","2030","2031","2032","2033","2034","2035","2036","2037"];
+const durationSlider = document.getElementById("myRange2");
+const labels =["2022","2023","2024","2025","2026","2027","2028","2029","2030","2031","2032","2033","2034","2035","2036","2037","2038","2039","2040","2041"];
 var selectedButton = "trustbtn";
 const growbtn = document.getElementById("growbtn");
 const trustbtn = document.getElementById("trustbtn");
 const pushbtn = document.getElementById("pushbtn");
 const mtlinvestval = document.getElementById("mtlinvestval");
 const durationval = document.getElementById("durationval");
-var slider = document.getElementById("myRange");
+const slider = document.getElementById("myRange");
+const totalAmount = document.getElementById("growthamount");
 
 document.getElementById(selectedButton).style.backgroundColor="rgb(214,214,214)";
 
 
   function slidersChanged(element){
-     const filledPercentage = 100*(element.value - element.min)/(element.max-element.min); 
+     const filledPercentage = 100 * (element.value - element.min)/(element.max-element.min); 
      element.style.backgroundImage = "linear-gradient(to right,#B7CB75 "+ filledPercentage + "%,rgb(214,214,214)"+  filledPercentage + "%)";
   }
 
@@ -45,14 +46,14 @@ document.getElementById(selectedButton).style.backgroundColor="rgb(214,214,214)"
    function percentagePerMonth(value){
       let numOfYears = durationSlider.value;
       console.log(numOfYears);
-      var savedAmount = Math.trunc(1071.42 * value / 100);
+      var savedAmount = (1071.428571 * value / 100);
       let amountPerYear = savedAmount * 12;
       var growthPercentage = 1.0321;
 
       switch(selectedButton){
         case "growbtn":
           {
-            growthPercentage = 1.0556;
+            growthPercentage = 1.0526;
             break;
           }
           case "pushbtn":
@@ -70,19 +71,25 @@ document.getElementById(selectedButton).style.backgroundColor="rgb(214,214,214)"
 
       }
       myChart.config.data.datasets[0].data = [];
-        var temp = amountPerYear;
-        for(let i = 0 ; i < numOfYears; i++)
+      var finalVal = 0;
+      for(let i = 0 ; i < numOfYears; i++)
+      {
+        var compoundInterest = 0;
+        for(let j = 0; j <= i ; j++)
         {
-          temp = growthPercentage * temp;
-          myChart.config.data.datasets[0].data.push(temp);
+          compoundInterest += Math.pow(growthPercentage,j)
         }
-        myChart.update();
+        finalVal = growthPercentage * amountPerYear * compoundInterest ;
+        myChart.config.data.datasets[0].data.push(finalVal);
       }
+      console.log(finalVal);
+      totalAmount.innerHTML = new Intl.NumberFormat("de-DE", {style: "currency", currency: "EUR"}).format(Math.trunc(finalVal * 100) / 100);
+      
+      myChart.update();
+    }
 
 
         
-
-
 const data = {
   labels : labels,
   datasets: [{
@@ -124,7 +131,7 @@ options: {
   },
   scales: {
     y: {
-      beginAtZero: true,
+      beginAtZero: false,
       display:false
     }
   }
